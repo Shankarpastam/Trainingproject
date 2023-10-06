5ff316200817234838aef137de63fa270529af46
Administrator <Administrator@172.31.40.38>
2023-10-06 06:17:56 +0000

    initial

---
+++ src/main/java/testclone/testproject.java
@@ -0,0 +1,14 @@
+package testclone;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.base.Strings;
+
+public class testproject {
+public static void main(Strings[]args){
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://signup.heroku.com/");
+	
+}
+}
